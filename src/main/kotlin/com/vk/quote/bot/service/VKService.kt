fc06ff83e40cec5f816d6596353a0b97500f2475
dmitryvasilkov <dmitryvasilkovw@gmail.com>
2025-03-29 23:52:02 +0300

    refactor: made the code idiomatic for the kotlin

--- src/main/kotlin/com/vk/quote/bot/service/VKService.kt
+++ src/main/kotlin/com/vk/quote/bot/service/VKService.kt
@@ -7,39 +7,40 @@ import org.springframework.http.RequestEntity
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.util.LinkedMultiValueMap
-import org.springframework.util.MultiValueMap
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 
 @Service
 class VkApiService(private val config: VkBotConfig) {
-    private val restTemplate = RestTemplate()
-    private val apiUrl = "https://api.vk.com/method"
+
     private val log = LoggerFactory.getLogger(VkApiService::class.java)
+    private val apiUrl = "https://api.vk.com/method"
+
+    private val restTemplate: RestTemplate = RestTemplate()
 
     fun sendMessage(userId: String, message: String) {
-        val url = UriComponentsBuilder.fromHttpUrl("$apiUrl/messages.send")
+        val url = UriComponentsBuilder.fromUriString("$apiUrl/messages.send")
             .queryParam("access_token", config.token)
             .queryParam("v", config.apiVersion)
+            .build()
             .toUriString()
 
-        val body: MultiValueMap<String, String> = LinkedMultiValueMap()
-        body.add("user_id", userId)
-        body.add("message", message)
-        body.add("random_id", System.currentTimeMillis().toString())
+        val body = LinkedMultiValueMap<String, String>().apply {
+            add("user_id", userId)
+            add("message", message)
+            add("random_id", System.currentTimeMillis().toString())
+        }
 
-        val headers = HttpHeaders()
-        headers.set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+        val headers = HttpHeaders().apply {
+            set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+        }
 
-        val requestEntity = RequestEntity
-            .post(url)
-            .headers(headers)
-            .body(body)
+        val requestEntity = RequestEntity.post(url).headers(headers).body(body)
 
-        try {
+        runCatching {
             val response: ResponseEntity<String> = restTemplate.exchange(requestEntity, String::class.java)
             log.info("Response: ${response.body}")
-        } catch (e: Exception) {
+        }.onFailure { e ->
             log.error("Error occurred: ${e.message}")
         }
     }
