85e49ddeb66f29cbebeef388c886769eed4673d6
dmitryvasilkov <dmitryvasilkovw@gmail.com>
2025-03-31 15:43:49 +0300

    refactor: code style

--- src/main/kotlin/com/vk/quote/bot/model/VkApiEndpoint.kt
+++ src/main/kotlin/com/vk/quote/bot/model/VkApiEndpoint.kt
@@ -2,5 +2,5 @@ package com.vk.quote.bot.model
 
 enum class VkApiEndpoint(val url: String) {
     BASE_URL("https://api.vk.com/method"),
-    GET_LONG_POLL_SERVER("${BASE_URL.url}/groups.getLongPollServer");
+    GET_LONG_POLL_SERVER("${BASE_URL.url}/groups.getLongPollServer"),
 }
